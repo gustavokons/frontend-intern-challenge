a017698bce9fe7860982693854321660f0cfe54d
gustavokons <gustavokons1@gmail.com>
2016-08-08 21:06:14 +0000

    Modificando arquivos com 'erros'

--- project/src/js/script.js
+++ project/src/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 		var total_hits = 0;
 		for(var i = 0; i < data.length; i++) {
 			if(i < 5) {
-				$(".ranking").append("<div class='row'><div class='col-sm-8'><p class='ranking-link'>"+data[i].shortUrl+"</p></div><div class='col-sm-4'><p class='ranking-clicks'>"+data[i].hits+"</p></div></div>");
+				$(".ranking").append("<div class='row'><div class='col-sm-8'><p class='ranking-link'><a href='"+data[i].shortUrl+"' target='_blank'>"+data[i].shortUrl+"</a></p></div><div class='col-sm-4'><p class='ranking-clicks'>"+data[i].hits+"</p></div></div>");
 			}
 			total_hits += data[i].hits;
  		}
